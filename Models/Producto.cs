d2955f536e0c55b1e5f7157d99f4cb779850c48e
Emiliano Alvarez <ems.alvarez@live.com.ar>
2026-02-23 16:24:57 -0300

    Creacion clase Producto, validaciones en setter y constructores

---
+++ Models/Producto.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SistemaStock.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+
+        // Aquí va tu modelo de negocio que ya tenés hecho
+    }
+}
